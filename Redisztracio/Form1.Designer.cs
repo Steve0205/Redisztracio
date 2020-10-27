5d00c673f87091f4bc5022c1cd25a425014c1d9b
Istvan <istvan780205@gmail.com>
2020-10-27 19:45:27 +0100

    1.0

---
+++ Redisztracio/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Redisztracio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox_nev = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton_ferfi = new System.Windows.Forms.RadioButton();
+            this.radioButton_no = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox_hobbi = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_ujhobbi = new System.Windows.Forms.TextBox();
+            this.button_hozzaad = new System.Windows.Forms.Button();
+            this.button_mentes = new System.Windows.Forms.Button();
+            this.button_betoltes = new System.Windows.Forms.Button();
+            this.dateTimePicker_szuletesdatum = new System.Windows.Forms.DateTimePicker();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // textBox_nev
+            // 
+            this.textBox_nev.Location = new System.Drawing.Point(112, 12);
+            this.textBox_nev.Name = "textBox_nev";
+            this.textBox_nev.Size = new System.Drawing.Size(165, 20);
+            this.textBox_nev.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Név:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Születési dátum:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nem:";
+            // 
+            // radioButton_ferfi
+            // 
+            this.radioButton_ferfi.AutoSize = true;
+            this.radioButton_ferfi.Checked = true;
+            this.radioButton_ferfi.Location = new System.Drawing.Point(60, 80);
+            this.radioButton_ferfi.Name = "radioButton_ferfi";
+            this.radioButton_ferfi.Size = new System.Drawing.Size(45, 17);
+            this.radioButton_ferfi.TabIndex = 5;
+            this.radioButton_ferfi.TabStop = true;
+            this.radioButton_ferfi.Text = "Férfi";
+            this.radioButton_ferfi.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_no
+            // 
+            this.radioButton_no.AutoSize = true;
+            this.radioButton_no.Location = new System.Drawing.Point(111, 80);
+            this.radioButton_no.Name = "radioButton_no";
+            this.radioButton_no.Size = new System.Drawing.Size(39, 17);
+            this.radioButton_no.TabIndex = 6;
+            this.radioButton_no.Text = "Nő";
+            this.radioButton_no.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(320, 14);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Kedvenc hobbi:";
+            // 
+            // listBox_hobbi
+            // 
+            this.listBox_hobbi.FormattingEnabled = true;
+            this.listBox_hobbi.Location = new System.Drawing.Point(323, 34);
+            this.listBox_hobbi.Name = "listBox_hobbi";
+            this.listBox_hobbi.Size = new System.Drawing.Size(99, 134);
+            this.listBox_hobbi.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(268, 189);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Új hobbi:";
+            // 
+            // textBox_ujhobbi
+            // 
+            this.textBox_ujhobbi.Location = new System.Drawing.Point(323, 186);
+            this.textBox_ujhobbi.Name = "textBox_ujhobbi";
+            this.textBox_ujhobbi.Size = new System.Drawing.Size(99, 20);
+            this.textBox_ujhobbi.TabIndex = 10;
+            // 
+            // button_hozzaad
+            // 
+            this.button_hozzaad.Location = new System.Drawing.Point(347, 222);
+            this.button_hozzaad.Name = "button_hozzaad";
+            this.button_hozzaad.Size = new System.Drawing.Size(75, 23);
+            this.button_hozzaad.TabIndex = 11;
+            this.button_hozzaad.Text = "Hozzáad";
+            this.button_hozzaad.UseVisualStyleBackColor = true;
+            this.button_hozzaad.Click += new System.EventHandler(this.button_hozzaad_Click);
+            // 
+            // button_mentes
+            // 
+            this.button_mentes.Location = new System.Drawing.Point(266, 266);
+            this.button_mentes.Name = "button_mentes";
+            this.button_mentes.Size = new System.Drawing.Size(75, 23);
+            this.button_mentes.TabIndex = 12;
+            this.button_mentes.Text = "Mentés";
+            this.button_mentes.UseVisualStyleBackColor = true;
+            this.button_mentes.Click += new System.EventHandler(this.button_mentes_Click);
+            // 
+            // button_betoltes
+            // 
+            this.button_betoltes.Location = new System.Drawing.Point(347, 266);
+            this.button_betoltes.Name = "button_betoltes";
+            this.button_betoltes.Size = new System.Drawing.Size(75, 23);
+            this.button_betoltes.TabIndex = 13;
+            this.button_betoltes.Text = "Betöltés";
+            this.button_betoltes.UseVisualStyleBackColor = true;
+            this.button_betoltes.Click += new System.EventHandler(this.button_betoltes_Click);
+            // 
+            // dateTimePicker_szuletesdatum
+            // 
+            this.dateTimePicker_szuletesdatum.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker_szuletesdatum.Location = new System.Drawing.Point(112, 46);
+            this.dateTimePicker_szuletesdatum.MinDate = new System.DateTime(1900, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker_szuletesdatum.Name = "dateTimePicker_szuletesdatum";
+            this.dateTimePicker_szuletesdatum.Size = new System.Drawing.Size(132, 20);
+            this.dateTimePicker_szuletesdatum.TabIndex = 14;
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "txt fájl|*.txt";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 324);
+            this.Controls.Add(this.dateTimePicker_szuletesdatum);
+            this.Controls.Add(this.button_betoltes);
+            this.Controls.Add(this.button_mentes);
+            this.Controls.Add(this.button_hozzaad);
+            this.Controls.Add(this.textBox_ujhobbi);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listBox_hobbi);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.radioButton_no);
+            this.Controls.Add(this.radioButton_ferfi);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_nev);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Regisztráció";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_nev;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton_ferfi;
+        private System.Windows.Forms.RadioButton radioButton_no;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox_hobbi;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox_ujhobbi;
+        private System.Windows.Forms.Button button_hozzaad;
+        private System.Windows.Forms.Button button_mentes;
+        private System.Windows.Forms.Button button_betoltes;
+        private System.Windows.Forms.DateTimePicker dateTimePicker_szuletesdatum;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Redisztracio/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Redisztracio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dateTimePicker_szuletesdatum.MaxDate = DateTime.Now;
+            StreamReader olvas = new StreamReader(@"lista.txt");
+
+            while(!olvas.EndOfStream)
+            {
+                listBox_hobbi.Items.Add(olvas.ReadLine());
+            }
+            olvas.Close();
+        }
+
+        private void button_hozzaad_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox_ujhobbi.Text.Trim()))
+            {
+                MessageBox.Show("Nem adta meg a hobbit!","Hibás adat!", MessageBoxButtons.OK, MessageBoxIcon.Error) ;
+                textBox_nev.Focus();
+            }
+            else
+            {
+                int listadb = listBox_hobbi.Items.Count;
+                int eredmeny = 0;
+                foreach(string item in listBox_hobbi.Items)
+                {
+                    if (textBox_ujhobbi.Text.ToUpper().TrimEnd().TrimStart() == item.ToUpper())
+                    {
+                        eredmeny = 1;
+                    }
+                }
+                if (eredmeny == 0)
+                {
+                    listBox_hobbi.Items.Add(textBox_ujhobbi.Text.TrimStart().TrimEnd());
+                    textBox_ujhobbi.Text = "";
+                    textBox_ujhobbi.Focus();
+                }
+                else
+                {
+                    MessageBox.Show("Már volt ilyen adat");
+                    textBox_ujhobbi.Focus();
+                }
+            }
+            
+
+            
+        }
+
+        private void button_mentes_Click(object sender, EventArgs e)
+        {
+            
+            
+            string nev="";
+            int hobbi = 0;
+            int ures = 0;
+            if (textBox_nev.Text.TrimStart().TrimEnd() == "")
+            {
+                MessageBox.Show("Hiányzik a név!", "Hiányos adat!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                ures = 1;
+            }
+            else
+            {
+                nev = textBox_nev.Text.TrimStart().TrimEnd();
+
+                
+            }
+            string szulev = dateTimePicker_szuletesdatum.Text;
+
+            string nem;
+            if(radioButton_ferfi.Checked)
+            {
+                nem = "ferfi";
+            }
+            else
+            {
+                nem = "no";
+            }
+            if (listBox_hobbi.SelectedIndex > -1)
+            {
+                hobbi = listBox_hobbi.SelectedIndex;
+            }
+            else
+            {
+                MessageBox.Show("Nem választottál ki hobbit", "Hibás adat", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                ures = 1;
+
+            }
+            if (ures == 0)
+            {
+                string egysor = nev + ";" + szulev + ";" + nem + ";" + hobbi;
+
+                saveFileDialog1.FileName = nev +".txt";
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    string fajl = saveFileDialog1.FileName;
+                    using (StreamWriter kiir = new StreamWriter(fajl))
+                    {
+                        kiir.WriteLine(egysor);
+                    }
+                    using (StreamWriter lista = new StreamWriter(@"lista.txt"))
+                    {
+                        foreach (string item in listBox_hobbi.Items)
+                        {
+                            lista.WriteLine(item);
+                        }
+                    }
+                    textBox_nev.Text = "";
+                    dateTimePicker_szuletesdatum.Text = DateTime.Now.ToString("yyyy.MM.dd.");
+                    radioButton_ferfi.Checked = true;
+                    listBox_hobbi.SelectedIndex = -1;
+                }
+                else
+                {
+                    return;
+                }
+                
+            }
+        }
+
+        private void button_betoltes_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                using (StreamReader beolvas = new StreamReader (openFileDialog1.FileName))
+                {
+                    while(!beolvas.EndOfStream)
+                    {
+                        string[] sor = beolvas.ReadLine().Split(';');
+                        textBox_nev.Text = sor[0];
+                        dateTimePicker_szuletesdatum.Text = sor[1];
+                        if(sor[2]=="ferfi")
+                        {
+                            radioButton_ferfi.Checked = true;
+                        }
+                        else
+                        {
+                            radioButton_no.Checked = true;
+                        }
+                        listBox_hobbi.SelectedIndex = int.Parse(sor[3]);
+                    }
+                    beolvas.Close();
+                }
+            }
+        }
+    }
+}
